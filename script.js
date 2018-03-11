5761f2660e148f8c9d6fdf713f3102312ccdef2b
Hayley <hayley@Hayleys-MacBook-Air.local>
2018-03-11 09:31:41 -0700

    More new stuff

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 let header = "<tr><th>Material</th><th>Type</th><th>min RH</th><th>max RH</th></tr>";
 // $("#table").append(header);
+let getColor = function(userNum, numMin, numMax) {
+  let color;
+  color = "table-warning";
+  if (userNum < numMin || numMax < userNum) {
+    color = "table-danger";
+  }
+  if (userNum > (numMin + 2) && userNum < (numMax - 2)) {
+    color = "table-success";
+  }
+  return color;
+};
 
 let createTable = function(rh) {
   let tableRows = '';
   for (let i = 0; i < materials.length; i = i + 1) {
-    let color;
     let row = materials[i];
-    color = "table-warning";
-    if (rh < row.minRH || row.maxRH < rh) {
-      color = "table-danger";
-    }
-    if (rh > row.minRH && rh < row.maxRH) {
-      color = "table-success";
-    }
+
     let rowHTML = `
-   <tr class=\"` + color + `\">
+   <tr class=\"` + getColor(rh, row.minRH, row.maxRH) + `\">
    <td>` + row.name + `</td>
    <td>` + row.type + `</td>
    <td>` + row.minRH + `%</td>
